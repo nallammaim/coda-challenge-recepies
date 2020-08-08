b82de091d3614691669ee82a7fa53a9f8d551853
nallammai <alagappan.maruthappan@verizonmedia.com>
2020-08-08 00:53:25 -0700

    Front end page

--- src/App.js
+++ src/App.js
@@ -2,25 +2,47 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      recepies: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://starlord.hackerearth.com/recipe").then((res) => res.json()).then((res) => this.setState({recepies:res}))
+  }
+
+
+  displayRecepie(recepie) {
+    return(<div key={recepie.id}>
+      <img src={recepie.image} className="App-img" />
+      <ul>
+        <li>Name: {recepie.name}</li>
+        <li>Category: {recepie.category}</li>
+        <li>Label: {recepie.label}</li>
+        <li>Price: {recepie.price}</li>
+        <li>Description: {recepie.description}</li>
+      </ul>
+    </div>)
+  }
+
+render() {
+  const { recepies } = this.state;
+  const recepieComponents = recepies.map((recepie) => this.displayRecepie(recepie));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Recepies!</h1>
+        <div className="grid-container">
+          {recepieComponents}
+        </div>
+      </div>
     </div>
   );
+}
 }
 
 export default App;
